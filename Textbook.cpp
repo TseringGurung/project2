81c980289a75d3eceae6059da00902ea9f5af202
Tsering Gurung <92239331+TseringGurung@users.noreply.github.com>
2023-02-21 16:50:17 -0500

    Update Textbook.cpp

--- Textbook.cpp
+++ Textbook.cpp
@@ -1,21 +1,20 @@
-#include <iostream>
-#include <string>
 #include "Textbook.hpp"
 
 Textbook::Textbook(){
-
+  book_subject_ = "";
+  book_grade_level_ = NONE;
+  book_review_questions = false;
 }
 
-Textbook::Textbook(std::string book_title, std::string book_author, int book_pages, bool is_digital, std::string book_subject, int book_grade_level, bool review_questions){
-
-}
+Textbook::Textbook(std::string title, std::string author, int page_count, std::string book_subject, bool is_digital = false, grade_level book_grade_level, bool review_questions = false):
+Book(title, author, page_count, is_digital), book_subject_(book_subject), book_grade_level_(book_grade_level), book_review_questions(review_questions){}
 
 /**
   @param  : a reference to a string representing the the subject of the book
   @post   : sets the private member variable to the value of the parameter
 **/
-void Textbook::setSubject(){
-
+void Textbook::setSubject(std::string& book_subject){
+  this->book_subject_ = book_subject;
 }
 
 
@@ -23,7 +22,7 @@ void Textbook::setSubject(){
   @return  : the subject of the textbook
 **/
 std::string Textbook::getSubject(){
-
+  return this->book_subject_;
 }
 
 
@@ -31,8 +30,8 @@ std::string Textbook::getSubject(){
   @param  : a reference to a grade_level
   @post   : sets the private member variable to the value of the parameter
 **/
-void Textbook::setGradeLevel(){
-
+void Textbook::setGradeLevel(grade_level& grade_level_){
+  this->book_grade_level_ = grade_level_;
 }
 
 
@@ -42,15 +41,16 @@ void Textbook::setGradeLevel(){
             it returns "JUNIOR HIGH")
 **/
 int Textbook::getGradeLevel(){
-
+  std::cout << "\"";
+  return this->book_grade_level_;
+  std::cout << "\"";
 }
-
 
 /**
   @post   : sets the review question flag to True
 **/
 void Textbook::setReviewQuestions(){
-
+  this->book_review_questions = true;
 }
 
 
@@ -58,5 +58,5 @@ void Textbook::setReviewQuestions(){
   @return  : the flag indicating if the texbook has review questions
 **/
 bool Textbook::checkReviewQuestions(){
-
+  return this->book_review_questions;
 }
