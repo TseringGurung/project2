2593fa9f6e06b8a99acc9626f49728b64574c6f2
Tsering Gurung <92239331+TseringGurung@users.noreply.github.com>
2023-02-21 17:50:27 -0500

    Update Manual.hpp

--- Manual.hpp
+++ Manual.hpp
@@ -52,8 +52,8 @@ class Manual : public Book{
     std::string setWebsite(const std::string& url_format);
     std::string getWebsite() const;
     void setVisualAid(const bool present_aid);
-    bool hasWebsite();
-    bool hasVisualAid();
+    bool hasWebsite() const;
+    bool hasVisualAid() const;
 };
 
 #endif
