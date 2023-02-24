098aa6b371d5b3cfaef5b3942734eaba6f9c932f
Tsering Gurung <92239331+TseringGurung@users.noreply.github.com>
2023-02-24 16:33:56 -0500

    Update Manual.cpp

--- Manual.cpp
+++ Manual.cpp
@@ -50,7 +50,7 @@ std::string Manual::getDevice() const{
              and either way set the has website flag to True
 **/
 bool Manual::setWebsite(const std::string &url_format){
-    std::regex regPattern("(https?://)([\\w\\d-]+\\.?)+[A-Za-z]{2,}(/[\\w\\d-]*)*");
+    std::regex regPattern("(https?://www\\.[a-zA-Z0-9]+\\.[a-zA-Z0-9]{2,})");
 
     if (std::regex_match(url_format, regPattern)) {
       url = url_format;
